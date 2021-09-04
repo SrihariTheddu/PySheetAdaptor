478d716bd341abe6f729a871855adf277aa56943
SrihariTheddu <56694426+SrihariTheddu@users.noreply.github.com>
2021-09-04 15:02:49 +0530

    Update PySheetAdaptor.py

--- src/PySheetAdaptor.py
+++ src/PySheetAdaptor.py
@@ -1,90 +1,119 @@
-##             ***         PYSHEET ADAPTOR         *****
-## *** AN EFFICIENT MODULE TO WORK WITH WORKSHEETS............
-## @Author       :: Theddu Srihari
-## @EmailID      :: sriharitheddu02@gmail.com.....
-## @Date         :: 11-09-2021
-## Requirements  :: pandas, numpy, matplotlib, os, sys modules........
-## Supported Versions..................................................
-## github link ::
-#=========================================================================================================================================================
-# *** Import all the required modules here...
 
+#=====================================================================================================================
 from PySheetAdaptor.src.Manager.PySheetManager import PySheetManager
 from PySheetAdaptor.src.client.GoogleClient import GoogleClient
 from sys import stdout, stderr
 from PySheetAdaptor.src.ResourceManager.utils import run_with_protected_environ
+from PySheetAdaptor.src.exceptions import BuildError, AuthenticationException, UploadError, SheetNotFoundException
+import os
 
-
-#=========================================================================================================================================================
-
+#======================================================================================================================
 class PySheetAdaptor(GoogleClient, PySheetManager):
 
-    ## *** checks the requirements satisfied.............
-    #@run_with_protected_environ
     def __init__(self):
-        ## *** set up the Runtime Environment
-        self.REPORT_LEVEL = 10
-        self.RECORD_LEVEL = 20
-        self.BOUNDED_ENVIRON = True
-        self.error_message = ""
-        self.credentials_url = None
-        try:
-            self.configure_environment()
-            stdout.write(f"Environment Launched successfully({self.BASE_DIR})\n")
-        except Exception as e:
-            stderr.write(f"oops!!! couldn't setup Environment")
-            stderr.write(f"{e}\n")
+        self.REPORT_LEVEL: int = 10
+        self.RECORD_LEVEL: int = 10
+        self.BOUNDED_ENVIRON: bool = False
+        self.error_message: str = ""
+        self.SERVER_SHEET: bool = False
+        self.adaptor = None
+        self.user_credentials_path: os.path = os.getcwd() + r"\guser\UserCredentials.json"
+        self.credentials_path = ""
+        self.credentials: dict = dict()
+        self.agree: bool = False
+        self.email_password: str = ""
+        self.email_id: str = ""
+        self.BASE_DIR: os.path = os.getcwd()
+        self.admin_password: str = ""
+        self.admin_name: str = ""
+        self.is_authorised: bool = False
+        self.logbookpath_url: str = ""
+        self.db_restorepath_url: str = ""
+        self.admin_credentials_url: str = ""
+        self.SHEET_DIR: str = ""
+        self.files: list = list()
+        self.progress_bar: bool = True
+        self.terms_and_conditions: str = f"""
+             Admin Name   : {self.admin_name}
+             Directory    : {os.getcwd()}
+             
+             This process has undertaken under the google guidelines and privacy policy of an individual user
+             This data is collected from your google cloud platform with the accessibity procedure
+             This software is allowed to view ,overwrite and change the data from the google cloud account...
+             google Account : {self.email_id}
+               ...............
+             """
 
+    def setup(self, *args, **kwargs) -> boolean:
+        """
+        it sets the local environment for working on spreadhseet....
+        It checks the python version, related packages are installed on your local system to work 
+        with the pysheet adaptor..
+        If Modules are not available then throws an Environmenttal setup error......
+        It checks whether you have user credentials (or) already you have a setup 
+        if yes :- it setup the logging system by using those credentials(like google client api credentials)
+        if not :- then it collects the information if the info is valid it setups the local environment.....
+        if locally inherits the PySheetResourceManager 
+        """
+        if not self.configure_environment():
+            raise EnvironmentError("couldn't setup environment")
+        stdout.write(f"Environment Launched successfully({self.BASE_DIR})\n")
+        return True
 
-    def connect(self, credentials_url=None):
-        if credentials_url is not None:
-            self.credentials_path = credentials_url
-        # *** Setting all the requirements..........
-        self.adaptor_setup(name="google_client")
+    def connect(self, *args, **kwargs) -> boolean:
+        """
+        It takes the credentials from your local files and authorizes it with by using google api
+        if invalid credentials then throws the Builderror........
+        """
+        if not self.adaptor_setup(name="google_client"):
+            raise BuildError("couldn't build the environment...")
         self.report.debug("loading client details .................")
-        self.client_setup(credentials_url=self.credentials_path)
+        if not self.client_setup(credentials_path=self.credentials_path):
+            raise AuthenticationException("couldn't setup the clientup.......")
         self.success.warning("Setup successfully.......")
+        return True
 
-
-    def load_from_server(self):
-        self.source_url = str(self.current_worksheet.url)
-        self.source_name = str(self.current_worksheet.title)
-        self._load_()
-        self._map_()
-        self.is_dumped = False
-
-    @run_with_protected_environ
-    def upload_to_server(self):
+    def upload_to_server(self) -> boolean:
         try:
             self.reload()
             self.sheet_clear()
+            self.back_up()
             self.current_worksheet.update(self.records)
             self.success.warning(f"Successfully uploaded to the Server url  : {self.source_url}")
             self.is_dumped = True
+            return True
         except Exception as e:
             self.error.error(f"OOps!!! could n't upload ... due to {e}")
             self.error.error("Your data may have corrupted........")
+            raise UploadError("couldn't upload.....")
 
-    @run_with_protected_environ
     def create_spreadsheet(self,spreadsheet_name : str = None) -> bool:
-        return super().create_spreadsheet(spreadsheet_name=spreadsheet_name)
+        if not super().create_spreadsheet(spreadsheet_name=spreadsheet_name):
+            raise SheetNotFoundException(f"Couldn't create the sheet with name {spreadsheet_name}") 
+        return True
 
-    #@run_with_status_bar
     def open_spreadsheet(self, **kwargs) -> bool:
-        return super().open_spreadsheet(title = kwargs.pop("title",None), url=kwargs.pop("url",None), key=kwargs.pop("key",None))
+        if not super().open_spreadsheet(title = kwargs.pop("title",None), url=kwargs.pop("url",None), key=kwargs.pop("key",None)):
+            raise SheetNotFoundException("no sheet found")
+        self.SERVER_SHEET = True
+        return True
 
     def create_worksheet(self, worksheet_name : str = None, rows:int = 100, cols : int = 100) -> bool:
-        super().create_worksheet(worksheet_name=worksheet_name, rows=rows, cols=cols)
-        self.install(self)
+        if super().create_worksheet(worksheet_name=worksheet_name, rows=rows, cols=cols):
+            self.install(self)
+        else:
+            raise SheetNotFoundException("Couldn't create the sheet")
 
     def open_worksheet(self, title: str = None, index:int = None) -> bool:
-        super().open_worksheet(title=title, index=index)
-        self.install(self)
-        self.setup_record_handler()
-        self.setup_backup()
-        self.load_from_server()
-        if self.no_of_rows == 0:
-            self.report.debug("*************** Worksheet Empty ***************** ")
+        if super().open_worksheet(title=title, index=index):
+            self.install(self)
+            self.setup_record_handler()
+            self.setup_backup()
+            self.load_from_server()
+            if self.no_of_rows == 0:
+                self.report.debug("*************** Worksheet Empty ***************** ")
+        else:
+            raise SheetNotFoundException(f"No Worksheet Found with title :: {title}")
 
     def set_columns(self, columns:list):
         return super().set_attributes(columns)
@@ -97,7 +126,6 @@ class PySheetAdaptor(GoogleClient, PySheetManager):
         self.report.debug(f"clearing the worksheet.............")
         self.current_worksheet.clear()
 
-
     def print_sheet(self):
         stdout.write(self.dataframe)
         stdout.write(f"\n To update the data please update the sheet.........")
@@ -107,20 +135,6 @@ class PySheetAdaptor(GoogleClient, PySheetManager):
         self.clear_environ()
         self.report.debug(" closing connection......")
         self = None
-
-    def set_path(self, *args, **kwargs):
-        path = None
-        if len(args)>1:
-            path = args[1]
-        else:
-            path = kwargs.pop("path",None)
-        if path is not None:
-            self.credentials_url = path
-            self.credentials_path = path
-            self.report.debug("path setup successfull.............")
-
-
-
 
 
 
